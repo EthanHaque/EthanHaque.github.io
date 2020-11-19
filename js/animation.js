b53b13445b82dd0984600dfe03db4341127364ca
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2020-11-19 00:33:19 -0600

    removing gui

--- js/animation.js
+++ js/animation.js
@@ -35,13 +35,13 @@ function init() {
     noise.seed(2);
 
     updateSettings(options);
-    gui = createGUI();
-    stats = new Stats();
-    stats.setMode(0);
-    stats.domElement.style.position = 'absolute';
-    stats.domElement.style.left = '0';
-    stats.domElement.style.top = '0';
-    document.body.appendChild(stats.domElement);
+    // gui = createGUI();
+    // stats = new Stats();
+    // stats.setMode(0);
+    // stats.domElement.style.position = 'absolute';
+    // stats.domElement.style.left = '0';
+    // stats.domElement.style.top = '0';
+    // document.body.appendChild(stats.domElement);
 
     renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
 
@@ -123,7 +123,7 @@ function importSettings() {
     changeDistance();
     updateLineRotation();
     updateLineColor();
-    updateGUI();
+    // updateGUI();
 }
 
 function updateGUI() {
@@ -185,12 +185,12 @@ function updateLineColor() {
 
 function updateBackgroundColor() {
     scene.background.set(options.backgroundColor);
-    updateGUI()
+    // updateGUI()
 }
 
 function changeDistance() {
     camera.position.set(options.horizontal, options.vertical, options.distanceFromScene * options.distanceScale);
-    updateGUI()
+    // updateGUI()
 }
 
 function createLine(pos) {
@@ -248,14 +248,14 @@ function updateLineMeshArray() {
         lineMeshes.push(line);
         scene.add(line);
     }
-    updateGUI();
+    // updateGUI();
 }
 
 function updateLinePosition() {
     for (var i = 0; i < lineMeshes.length; i++) {
         lineMeshes[i].position.y = options.spacingFactor * (i - options.numOfLines / 2);
     }
-    updateGUI()
+    // updateGUI()
 }
 
 function onWindowResize() {
@@ -282,7 +282,7 @@ function render(time) {
         lineMeshes[j].geometry.attributes.position.needsUpdate = true;
     }
 
-    stats.update();
+    // stats.update();
 }
 
 function animate(time) {
