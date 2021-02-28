9f84313957b57daaffe6bf817af819d04009d11b
Ethan Haque <43977335+EthanHaque@users.noreply.github.com>
2021-02-28 17:20:14 -0600

    changing time

--- proj/timer/js/timer.js
+++ proj/timer/js/timer.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-    let birthday = "Feb 28, 2021 17:05:00",
+    let birthday = "Feb 28, 2021 17:55:00",
         countDown = new Date(birthday).getTime(),
         x = setInterval(function () {
 
